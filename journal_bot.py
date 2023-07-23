60394f0291dfb9262dee9a5dc864042de80f2f78
joshsisto <josh@joshsisto.com>
2023-07-22 22:10:32 -0700

    added new summary features

--- journal_bot.py
+++ journal_bot.py
@@ -1,6 +1,7 @@
 import openai
 import os
-from datetime import datetime
+from datetime import datetime,timedelta
+import glob
 
 from utilities import get_today, get_now, red, blue, bold 
 
@@ -79,3 +80,179 @@ def chatbot():
             messages.append({"role": "assistant", "content": assistant_message})
             f.write("Assistant: " + assistant_message + "\n\n")
             print("Assistant: ", blue(assistant_message))
+
+
+def summarize_all_files():
+    # Load the summarization prompt
+    with open('./prompts/summary_prompt.txt', 'r') as f:
+        summary_prompt = f.read()
+
+    # Scan the logs directory for all .all files
+    all_files = glob.glob("./logs/*/*.all")
+
+    for file_path in all_files:
+        # Check if corresponding .summ file already exists
+        summary_filename = file_path.replace('.all', '.summ')
+        if os.path.exists(summary_filename):
+            print(f"Skipping {file_path} as {summary_filename} already exists.")
+            continue
+
+        # Read the content of the .all file
+        with open(file_path, 'r') as f:
+            content = f.read()
+
+        # Concatenate the prompt and the content
+        conversation = summary_prompt + "\n" + content
+
+        # Prepare the initial message to send to the assistant
+        messages = [
+            {"role": "system", "content": conversation},
+        ]
+
+        # Get the summary from the assistant
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=messages,
+            temperature=0.8,
+            max_tokens=500,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0.6
+        )
+        summary = response.choices[0].message['content']
+
+        # Save the summary to a .summ file
+        with open(summary_filename, 'w') as f:
+            f.write(summary)
+
+    print("Summarization completed for all applicable .all files.")
+
+
+def weekly_summary():
+    # Load the weekly summarization prompt
+    with open('./prompts/weekly_summary_prompt.txt', 'r') as f:
+        weekly_summary_prompt = f.read()
+
+    # Get all .summ files and sort them
+    summ_files = sorted(glob.glob("./logs/*/*.summ"))
+
+    while summ_files:
+        # Take last 7 .summ files for a week
+        weekly_files = summ_files[-7:]
+        del summ_files[-7:]
+
+        # Aggregate content from these .summ files
+        aggregated_content = ""
+        for file_path in weekly_files:
+            with open(file_path, 'r') as f:
+                aggregated_content += f.read() + "\n\n"
+
+        # Concatenate the prompt and the aggregated content
+        conversation = weekly_summary_prompt + "\n" + aggregated_content
+
+        # Prepare the initial message to send to the assistant
+        messages = [
+            {"role": "system", "content": conversation},
+        ]
+
+        # Get the weekly summary from the assistant
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=messages,
+            temperature=0.8,
+            max_tokens=1500,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0.6
+        )
+        summary = response.choices[0].message['content']
+
+        # Extract the date range based on the filenames in the chunk
+        start_date_str = os.path.basename(weekly_files[0]).replace('.summ', '')
+        end_date_str = os.path.basename(weekly_files[-1]).replace('.summ', '')
+        date_range_str = f"{start_date_str}_to_{end_date_str}"
+
+        # Save the weekly summary to a .week file in the root of the /logs/ folder
+        weekly_filename = f"./logs/{date_range_str}.week"
+        with open(weekly_filename, 'w') as f:
+            f.write(summary)
+
+        print(f"Weekly summary saved to {weekly_filename}.")
+
+
+def count_tokens(text):
+    """Utility function to count tokens in a given text."""
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k",
+        messages=[{"role": "user", "content": text}],
+    )
+    return response.usage['total_tokens']
+
+
+def total_summary():
+    # Load the total summarization prompt
+    with open('./prompts/total_summary_prompt.txt', 'r') as f:
+        total_summary_prompt = f.read()
+
+    prompt_tokens = count_tokens(total_summary_prompt)
+    max_tokens = 10000 - prompt_tokens  # Adjusting for the prompt's token count
+
+    # Get all .summ files and sort them
+    summ_files = sorted(glob.glob("./logs/*/*.summ"))
+
+    aggregated_content = ""
+    token_count = 0
+    start_file = summ_files[0]
+
+    for file_path in summ_files:
+        with open(file_path, 'r') as f:
+            content = f.read()
+            tokens = count_tokens(content)
+            
+            if token_count + tokens > max_tokens:
+                # Save the aggregated content as .sum and reset
+                end_file = file_path
+                save_summary(aggregated_content, start_file, end_file, total_summary_prompt)
+                
+                # Reset aggregation and token count
+                aggregated_content = content + "\n\n"
+                token_count = tokens
+                start_file = file_path
+            else:
+                aggregated_content += content + "\n\n"
+                token_count += tokens
+
+    # Handle any remaining content after the loop ends
+    if aggregated_content:
+        end_file = summ_files[-1]
+        save_summary(aggregated_content, start_file, end_file, total_summary_prompt)
+
+
+def save_summary(aggregated_content, start_file, end_file, total_summary_prompt):
+    """Utility function to save the summary based on content and date range."""
+    conversation = total_summary_prompt + "\n" + aggregated_content
+    messages = [{"role": "system", "content": conversation}]
+    
+    # Adjusting the max tokens to ensure total tokens (input + output) is within model's limit
+    max_response_tokens = 14385 - len(conversation.split())  # Assuming one word = one token for simplicity
+    
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k",
+        messages=messages,
+        temperature=0.8,
+        max_tokens=max_response_tokens,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6
+    )
+    summary = response.choices[0].message['content']
+
+    start_date_str = os.path.basename(start_file).replace('.summ', '')
+    end_date_str = os.path.basename(end_file).replace('.summ', '')
+    date_range_str = f"{start_date_str}_to_{end_date_str}"
+
+    total_filename = f"./logs/{date_range_str}.sum"
+    with open(total_filename, 'w') as f:
+        f.write(summary)
+
+    print(f"Total summary saved to {total_filename}.")
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from journal import create_journal_entry, guided_journal_entry, read_journal_entry, consolidate_files
 from stats import create_stats_entry
 from todo import manage_todo_list
-from journal_bot import chatbot
+from journal_bot import chatbot, summarize_all_files, weekly_summary, total_summary
 
 def main():
     while True:
-        print("\n1. New journal entry\n2. Guided journal entry\n3. Read a previous journal\n4. To-do list\n5. New stats entry\n6. chatbot\n7. Exit")
+        print("\n1. New journal entry\n2. Guided journal entry\n3. Read a previous journal\n4. To-do list\n5. New stats entry\n6. chatbot\n7. Summarize\n8. weekly\n9. Life Story!\n10. Exit")
         choice = int(input("Select an option: "))
         if choice == 1:
             create_journal_entry()
@@ -15,6 +15,7 @@ def main():
             guided_journal_entry()
             manage_todo_list()
             consolidate_files()
+            chatbot()
         elif choice == 3:
             read_journal_entry()
         elif choice == 4:
@@ -26,6 +27,12 @@ def main():
             chatbot()
             consolidate_files()
         elif choice == 7:
+            summarize_all_files()
+        elif choice == 8:
+            weekly_summary()
+        elif choice == 9:
+            total_summary()
+        elif choice == 10:
             break
         else:
             print("Invalid choice. Please choose a number from 1 to 7.")
